d77f8155b9f7fb5edd93f200d8a21737cfd12f80
Bogdan Ghit <bogdan.ghit@databricks.com>
2020-02-13 13:07:56 +0100

    Load ODBC extras

--- tableau-databricks/connection-builder.js
+++ tableau-databricks/connection-builder.js
@@ -40,7 +40,20 @@ limitations under the License.
 	// Minimum interval between consecutive polls for query execution status (1ms)
 	params["AsyncExecPollInterval"] = "1";
 
-	var formattedParams = [];
+  // Load ODBC connection string extras
+  var odbcConnectStringExtrasMap = {};
+	const attributeODBCConnectStringExtras = "odbc-connect-string-extras";
+
+	if (attributeODBCConnectStringExtras in attr) {
+		odbcConnectStringExtrasMap = connectionHelper
+			.ParseODBCConnectString(attr[attributeODBCConnectStringExtras]);
+	}
+
+  for (var key in odbcConnectStringExtrasMap) {
+		params[key] = odbcConnectStringExtrasMap[key];
+	}
+
+  var formattedParams = [];
   formattedParams.push(
 		connectionHelper.formatKeyValuePair(
 			driverLocator.keywordDriver, driverLocator.locateDriver(attr)));
