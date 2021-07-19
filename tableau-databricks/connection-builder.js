b709b33fb225bbcf9ecfe1ea5bc4a26fa0ad1fd3
Bogdan Ghit <bogdan.ghit@databricks.com>
2021-07-19 21:41:50 +0200

    Fix indentation

--- tableau-databricks/connection-builder.js
+++ tableau-databricks/connection-builder.js
@@ -34,7 +34,7 @@ limitations under the License.
 	if (attr[connectionHelper.attributeDatabase] &&
 	    attr[connectionHelper.attributeDatabase] !== "SPARK") {
 		params["CATALOG"] = attr[connectionHelper.attributeDatabase];
-   	params["SSP_databricks.catalog"] = attr[connectionHelper.attributeDatabase];
+		params["SSP_databricks.catalog"] = attr[connectionHelper.attributeDatabase];
   }
 
 	var authenticationMode = attr[connectionHelper.attributeAuthentication];
