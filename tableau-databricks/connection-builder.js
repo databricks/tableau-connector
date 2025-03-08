3e5584b4c59a15727a532847eaf33f7bb1574b8b
Eric Wang <e.wang@databricks.com>
2025-03-07 23:13:50 -0800

    update

--- tableau-databricks/connection-builder.js
+++ tableau-databricks/connection-builder.js
@@ -58,8 +58,8 @@ limitations under the License.
 			break;
 
 		case "auth-client-creds":
-			params["AUTHMECH"] = 11;
-			params["AUTH_FLOW"] = 1;
+			params["AuthMech"] = 11;
+			params["Auth_Flow"] = 1;
 			params["Auth_Client_ID"] = attr[connectionHelper.attributeUsername];
 			params["Auth_Client_Secret"] = attr[connectionHelper.attributePassword];
 			params["Auth_Scope"] = "sql";
