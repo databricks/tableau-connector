1613a283d9e3540ef2c5d800ffde2712ca446078
Sreekanth Vadigi <sreekanth.vadigi@databricks.com>
2025-08-28 15:17:37 +0530

    updated attr v-query-tags
    Signed-off-by: Sreekanth Vadigi <sreekanth.vadigi@databricks.com>

--- tableau-databricks/connection-builder.js
+++ tableau-databricks/connection-builder.js
@@ -20,7 +20,7 @@ limitations under the License.
 
  	var vendorDefined = {
 		attributeHttpPath: "v-http-path",
-		attributeQueryTags: "query-tags"
+		attributeQueryTags: "v-query-tags"
 	}
 
 	// The Databricks cluster ODBC endpoint
